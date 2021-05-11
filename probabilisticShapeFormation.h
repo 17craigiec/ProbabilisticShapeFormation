656ab601c1744c1fb5c06af2a2abb88173d9b176
nstallings <nstallings44@comcast.net>
2021-05-10 20:04:25 -0400

    cmake loop files for data collection

---
+++ probabilisticShapeFormation.h
@@ -0,0 +1,72 @@
+#ifndef PROBABILISTICSHAPEFORMATION_H
+#define PROBABILISTICSHAPEFORMATION_H
+
+#include <buzz/argos/buzz_loop_functions.h>
+#include <argos3/core/utility/math/rng.h>
+
+class CProbabilisticShapeFormation : public CBuzzLoopFunctions {
+
+public:
+
+   CProbabilisticShapeFormation() {}
+   virtual ~CProbabilisticShapeFormation() {}
+
+   /**
+    * Executes user-defined initialization logic.
+    * @param t_tree The 'loop_functions' XML configuration tree.
+    */
+   virtual void Init(TConfigurationNode& t_tree);
+
+   /**
+    * Executes user-defined reset logic.
+    * This method should restore the state of the simulation at it was right
+    * after Init() was called.
+    * @see Init()
+    */
+   virtual void Reset();
+
+   /**
+    * Executes user-defined logic right after a control step is executed.
+    */
+   virtual void PostStep();
+   
+   /**
+    * Returns true if the experiment is finished, false otherwise.
+    *
+    * This method allows the user to specify experiment-specific ending
+    * conditions. If this function returns false and a time limit is set in the
+    * .argos file, the experiment will reach the time limit and end there. If no
+    * time limit was set, then this function is the only ending condition.
+    *
+    * @return true if the experiment is finished.
+    */
+   virtual bool IsExperimentFinished();
+
+   /**
+    * Executes user-defined destruction logic.
+    * This method should undo whatever is done in Init().
+    * @see Init()
+    */
+   virtual void Destroy();
+
+   virtual void BuzzBytecodeUpdated();
+
+private:
+
+   int GetNumRobots() const;
+
+private:
+   /** The stimuli associated to the tasks */
+   std::vector<Real> m_vecStimuli;
+
+   /** The output file name */
+   std::string m_strOutFile;
+
+   /** The output file stream */
+   std::ofstream m_cOutFile;
+
+   /** Random number generator */
+   CRandom::CRNG* m_pcRNG;
+};
+
+#endif
